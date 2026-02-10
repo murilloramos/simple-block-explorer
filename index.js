9842d542a045780805e554e68a80b5e3c77d629a
Murillo Ramos <murillo.tech.dev@gmail.com>
2026-02-10 20:11:22 -0300

    Showing latest blocks with blockstream api

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("The Bitcoin Explorer is running!!!")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
+
+app.get("/blocks", async (req, res) => {
+    try {
+        const response = await axios.get(
+            "https://blockstream.info/api/blocks"
+        )
+        res.json(response.data)
+    } catch (err) {
+        res.status(500).send("Error fetching blocks")
+    }
+})
