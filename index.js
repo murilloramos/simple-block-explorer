0e30d8ae43957849884189ae85fe3d701415234b
Murillo Ramos <murillo.tech.dev@gmail.com>
2026-02-10 21:08:42 -0300

    View a single block data

--- index.js
+++ index.js
@@ -22,3 +22,16 @@ app.get("/blocks", async (req, res) => {
         res.status(500).send("Error fetching blocks")
     }
 })
+
+app.get("/block/:hash", async (req, res) => {
+    const { hash } = req.params
+
+    try {
+        const response = await axios.get(
+            `https://blockstream.info/api/block/${hash}`
+        )
+        res.json(response.data)
+    } catch (err) {
+        res.status(500).send("Error fetching block")
+    }
+})
